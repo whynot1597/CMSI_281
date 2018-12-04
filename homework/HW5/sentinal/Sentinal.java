8b90a9ecf91fe4c1e369de0ad40e9c2698f4f088
whynot1597 <tahoesoccerdefender@yahoo.com>
2018-12-04 12:24:13 -0800

    new new

--- homework/HW5/sentinal/Sentinal.java
+++ homework/HW5/sentinal/Sentinal.java
@@ -1,22 +1,35 @@
 package sentinal;
- import java.io.File;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
- public class Sentinal implements SentinalInterface {
-     // -----------------------------------------------------------
+
+public class Sentinal implements SentinalInterface {
+
+    // -----------------------------------------------------------
     // Fields
     // -----------------------------------------------------------
-     private PhraseHash posHash, negHash;
-     // -----------------------------------------------------------
+
+    private PhraseHash posHash, negHash;
+
+
+    // -----------------------------------------------------------
     // Constructor
     // -----------------------------------------------------------
-     Sentinal (String posFile, String negFile) throws FileNotFoundException {
-        // TODO: load files into PhraseHash fields appropriately
+
+    Sentinal (String posFile, String negFile) throws FileNotFoundException {
+    	posHash = new PhraseHash();
+    	negHash = new PhraseHash();
+    	loadSentimentFile(posFile, true);
+        loadSentimentFile(negFile, false);
     }
-     // -----------------------------------------------------------
+
+
+    // -----------------------------------------------------------
     // Methods
     // -----------------------------------------------------------
-     public void loadSentiment (String phrase, boolean positive) {
+
+    public void loadSentiment (String phrase, boolean positive) {
         if (positive) {
         	posHash.put(phrase);
         	return;
@@ -24,19 +37,55 @@ import java.util.Scanner;
         
         negHash.put(phrase);
     }
-     public void loadSentimentFile (String filename, boolean positive) throws FileNotFoundException {
+
+    public void loadSentimentFile (String filename, boolean positive) throws FileNotFoundException {
         Scanner reader = new Scanner(new File (filename));
         while (reader.hasNextLine()) {
         	loadSentiment(reader.nextLine(), positive);
         }
-        loadSentiment(reader.nextLine(), positive);
         reader.close();
     }
-     public String sentinalyze (String filename) throws FileNotFoundException {
-        throw new UnsupportedOperationException();
+
+    public String sentinalyze (String filename) throws FileNotFoundException {
+        int totalCount = count(filename, true) - count(filename, false);
+        if (totalCount > 0) {
+        	return "positive";
+        }
+        if (totalCount < 0) {
+        	return "negative";
+        }
+        return "neutral";
+        
     }
-     // -----------------------------------------------------------
-    // Helper Methods
-    // -----------------------------------------------------------
-     // TODO: add your helper methods here!
- }
+    
+    private int count(String filename, boolean positive) throws FileNotFoundException {
+    	int count = 0;
+    	PhraseHash hash;
+    	if (positive) { 
+    		hash = posHash;
+    	} else {
+    		hash = negHash;
+    	}
+    	Scanner reader = new Scanner(new File(filename));
+    	for (int index = 0; reader.hasNext(); index++, reader.next()) {
+    		Scanner innerReader = new Scanner(new File(filename));
+    		for (int s = 0; s < index; s++) {
+    			innerReader.next();
+    		}
+    		String test = "";
+    		for (int i = 0; i < hash.longestLength() && innerReader.hasNext(); i++) {
+    			if (test.equals("")) {
+    				test = innerReader.next();
+    			} else {
+    				test = test + " " + innerReader.next();
+    			}
+    			if (hash.get(test) != null) {
+    				count++;
+    			}
+    		}
+    		innerReader.close();
+    	}
+    	reader.close();
+    	return count;
+    }
+}
